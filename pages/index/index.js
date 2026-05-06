8707aa41e96656c2904084ee8059400daba6c366
chenyiming <1990383281@qq.com>
2026-05-06 17:00:38 +0800

    Revert "Feature/page"

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,17 @@
+const { webviewUrl } = require('../../utils/config.js');
+
 Page({
-  data: {},
+  data: {
+    hasWebview: false,
+    skyStars: [],
+    reflectionStars: [],
+  },
+
+  onLoad() {
+    this.setData({
+      hasWebview: !!(webviewUrl && String(webviewUrl).trim()),
+    });
+  },
 
   goExhibit() {
     wx.navigateTo({
